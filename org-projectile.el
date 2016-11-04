7c68ebf4f6cee72c0f7d90f9d253347047eccf01
Ivan Malison <IvanMalison@gmail.com>
2016-11-03 18:42:09 -0700

    Version bump

--- org-projectile.el
+++ org-projectile.el
@@ -5,7 +5,7 @@
 ;; Author: Ivan Malison <IvanMalison@gmail.com>
 ;; Keywords: org projectile todo
 ;; URL: https://github.com/IvanMalison/org-projectile
-;; Version: 0.2.4
+;; Version: 0.2.5
 ;; Package-Requires: ((projectile "0.11.0") (dash "2.10.0") (emacs "24") (pcache "0.4.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
