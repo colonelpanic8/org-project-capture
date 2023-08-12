244f80d7012b180f60ee5ee2d47f5307e4f831b2
Ivan Malison <IvanMalison@gmail.com>
2023-08-12 15:41:14 -0600

    Add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cask
+          gnumake
+        ];
+      };
+    });
+}
